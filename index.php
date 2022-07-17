98760c87381565c1f5c9cd23200071fd12fcb908
keikasuzuki <keika.suzuki.24@gmail.com>
2022-07-17 16:29:46 +0900

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>じゃんけんゲーム</title>
+</head>
+<body>
+	<form action="result.php" method="post">
+	じゃんけんをしよう<br>
+	<select name= "hand">
+		<option value = "グー">グー</option>
+		<option value = "チョキ">チョキ</option>
+		<option value = "パー">パー</option>
+	</select><br>
+	<input type="submit" value="送信">
+	</form>
+</body>
+</html>
---
+++ result.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>じゃんけんゲーム結果</title>
+</head>
+<body>
+	<?php
+		$your_choice = $_POST['hand'].'<br>';
+		
+		$pc_hand = ["グー", "チョキ", "パー"];
+		
+		$count = count($pc_hand);
+		
+		$number = rand(0, $count - 1);
+		
+		$rand_choice = $pc_hand[$number];
+
+		if ($your_choice == $rand_choice) {
+			$result ='あいこ';
+			var_dump($your_choice);
+			var_dump($rand_choice);
+		} elseif ($your_choice == 'グー' && $rand_choice == 'チョキ') {
+				$result = '勝ち';
+				var_dump($your_choice);
+				var_dump($rand_choice);
+		} elseif ($your_choice == 'チョキ' && $rand_choice == 'パー') {
+				$result = '勝ち';
+				var_dump($your_choice);
+				var_dump($rand_choice);
+		} elseif ($your_choice == 'パー' && $rand_choice == 'グー') {
+				$result = '勝ち';
+				var_dump($your_choice);
+				var_dump($rand_choice);
+		} else {
+				$result = '負け';
+		}
+
+		echo '自分： '.$your_choice;
+		echo '相手： '.$rand_choice.'<br>';
+		echo '結果： '.$result;
+		
+	?>
+	<form action="index.php">
+		<input type="submit" value="再勝負">
+	</form>
+</body>
+</html>
